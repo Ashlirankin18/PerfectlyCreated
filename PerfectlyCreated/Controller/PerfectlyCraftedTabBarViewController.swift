a91ab51373e6562241be18cdcb03aed2aa108273
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-10-31 11:00:55 -0400

    Updated Tabbar items

--- PerfectlyCreated/Controller/PerfectlyCraftedTabBarViewController.swift
+++ PerfectlyCreated/Controller/PerfectlyCraftedTabBarViewController.swift
@@ -8,47 +8,48 @@
 
 import UIKit
 
-class PerfectlyCraftedTabBarViewController: UITabBarController {
-  
-  var allHairProducts = [AllHairProducts]()
-  var theLocalUser:UserModel?
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.view.backgroundColor = #colorLiteral(red: 0.6722700215, green: 1, blue: 0.6019102933, alpha: 1)
-    getAllHairProducts()
-    setUpTabbarItems()
+final class PerfectlyCraftedTabBarViewController: UITabBarController {
+    
+    var allHairProducts = [AllHairProducts]()
+    
+    var theLocalUser:UserModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.6722700215, green: 1, blue: 0.6019102933, alpha: 1)
+        getAllHairProducts()
+        setUpTabbarItems()
     }
-
-
-  private func setUpTabbarItems(){
-    let feedsViewController = FeedsViewController()
-    let profileViewController = ProfileViewController.init(view: ProfileView())
-    let profileNavigationController = UINavigationController(rootViewController: profileViewController)
-    let newsFeedNavigationController = UINavigationController(rootViewController: feedsViewController)
-    let searchViewController = SearchProductViewController()
-    let searchNavigationController = UINavigationController(rootViewController: searchViewController)
-    let myProductViewController = UIStoryboard.init(name: "ProfileOptions", bundle: nil).instantiateViewController(withIdentifier: "HairProductsTableViewController")
-    feedsViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-hashtag-activity-feed-25")
-    feedsViewController.title = "News Feed"
-    profileViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-user-26")
-    profileViewController.title = "Profile"
-    searchViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-search-25")
-    searchViewController.title = "Search"
-    myProductViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-spray-filled-25.png")
-      myProductViewController.title = "My Products"
     
-    self.viewControllers = [newsFeedNavigationController,searchNavigationController,myProductViewController, profileNavigationController]
     
-  }
-  private func getAllHairProducts(){
-    HairProductApiClient.getHairProducts { (error, allHairProducts) in
-      if let error = error {
-        print(error.errorMessage())
-      }
-      if let allHairProducts = allHairProducts{
-    ProductDataManager.setProducts(products: allHairProducts)
-      }
+    private func setUpTabbarItems() {
+        
+        let profileViewController = ProfileViewController.init(view: ProfileView())
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        
+        let searchViewController = SearchProductViewController()
+        let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+        let myProductViewController = UIStoryboard.init(name: "ProfileOptions", bundle: nil).instantiateViewController(withIdentifier: "HairProductsTableViewController")
+        
+        profileViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-user-26")
+        profileViewController.title = "Profile"
+        searchViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-search-25")
+        searchViewController.title = "Search"
+        myProductViewController.tabBarItem.image = #imageLiteral(resourceName: "icons8-spray-filled-25.png")
+        myProductViewController.title = "My Products"
+        
+        self.viewControllers = [myProductViewController, searchNavigationController, profileNavigationController]
+        
     }
-  }
     
+    private func getAllHairProducts() {
+        HairProductApiClient.getHairProducts { (error, allHairProducts) in
+            if let error = error {
+                print(error.errorMessage())
+            }
+            if let allHairProducts = allHairProducts{
+                ProductDataManager.setProducts(products: allHairProducts)
+            }
+        }
+    }
 }

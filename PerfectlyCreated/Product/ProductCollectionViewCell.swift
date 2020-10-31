6f7496ade6cdbe96e91d15f5e83f498877e59545
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-10-31 13:05:26 -0400

    Product cell

---
+++ PerfectlyCreated/Product/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 10/31/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var productImageView: UIImageView!
+    @IBOutlet private weak var productNameLabel: UILabel!
+    
+    
+}
---
+++ PerfectlyCreated/Product/ProductViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductViewController.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 10/31/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+final class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

9fbffd7cef8033f31f5515716662ea3a06f1ee3e
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-10-31 13:46:04 -0400

    Added alert controller

--- PerfectlyCreated/Product/ProductViewController.swift
+++ PerfectlyCreated/Product/ProductViewController.swift
@@ -23,8 +23,30 @@ final class ProductViewController: UIViewController {
     
     private func configureButtonTapHandler() {
         addBarButtonItem.tapPublisher.sink { [weak self] _ in
-            
+            self?.showAlertController()
         }
         .store(in: &cancellables)
+    }
+    
+    private func showAlertController() {
+        let alertController = UIAlertController(title: "Add Product", message: nil, preferredStyle: .actionSheet)
+        
+        let uploadProductAction = UIAlertAction(title: "Upload barcode", style: .default) { _ in
+            
+        }
+        
+        let scanBarCodeAction = UIAlertAction(title: "Scan barcode", style: .default) { _ in
+            
+        }
+        
+       let searchProduct = UIAlertAction(title: "Search", style: .default) { _ in
+            
+       }
+        
+        alertController.addAction(uploadProductAction)
+        alertController.addAction(scanBarCodeAction)
+        alertController.addAction(searchProduct)
+        
+        present(alertController, animated: true)
     }
 }

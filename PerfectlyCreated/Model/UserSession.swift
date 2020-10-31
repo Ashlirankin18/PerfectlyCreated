6c4a8f19c48d768efa2758e137f0683e11a9d414
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-10-31 10:38:03 -0400

    Updated user session and properties

--- PerfectlyCreated/Model/UserSession.swift
+++ PerfectlyCreated/Model/UserSession.swift
@@ -10,100 +10,72 @@ import Foundation
 import FirebaseAuth
 
 protocol UserSessionAccountCreationDelegate:AnyObject {
-  func didReceiveError(_ userSession: UserSession, error:Error)
-  func didCreateAccount(_ userSession:UserSession,user:User)
+    func didReceiveError(_ userSession: UserSession, error:Error)
+    func didCreateAccount(_ userSession:UserSession,user:User)
 }
 protocol UserSessionSignOutDelegate:AnyObject{
-  func didReceiveSignOutError(_ userSession:UserSession,error:Error)
-  func didSignOutUser(_userSession:UserSession)
+    func didReceiveSignOutError(_ userSession:UserSession,error:Error)
+    func didSignOutUser(_userSession:UserSession)
 }
 protocol UserSessionSignInExistingUserDelegate:AnyObject {
-  func didReceiveSignInError(_ userSession: UserSession,error:Error)
-  func didSignInUser(_ userSession: UserSession, user: User)
+    func didReceiveSignInError(_ userSession: UserSession,error:Error)
+    func didSignInUser(_ userSession: UserSession, user: User)
 }
 
-class UserSession {
-  weak var userSessionAccountdelegate: UserSessionAccountCreationDelegate?
-  weak var userSessionSignOutDelegate: UserSessionSignOutDelegate?
-  weak var userSignInDelegate: UserSessionSignInExistingUserDelegate?
-  
-
- public func createUser(email:String,password:String){
-    Auth.auth().createUser(withEmail: email, password: password) { (authDataResults, error) in
-      if let error = error{
-        self.userSessionAccountdelegate?.didReceiveError(self, error: error)
-      }
-      else if let authDataResults = authDataResults{
-    self.userSessionAccountdelegate?.didCreateAccount(self, user: authDataResults.user)
-        DataBaseManager.firebaseDB.collection(FirebaseCollectionKeys.users)
-        .document(authDataResults.user.uid.description)
-        .setData(["userName" : "",
-                  "hairType": "",
-                  "bio": "",
-                  "email": authDataResults.user.email ?? "",
-                  "userId": authDataResults.user.uid,
-                  "imageURL": ""
-          ], completion: { (error) in
+final class UserSession {
+    
+    weak var userSessionAccountdelegate: UserSessionAccountCreationDelegate?
+    weak var userSessionSignOutDelegate: UserSessionSignOutDelegate?
+    weak var userSignInDelegate: UserSessionSignInExistingUserDelegate?
+    
+    private var documentId: String {
+        return DataBaseManager.firebaseDB.collection(FirebaseCollectionKeys.users).document().documentID
+    }
+    
+    func createUser(email: String, password: String, username: String) throws {
+        let userId = documentId
+        let user = UserModel(userName: username, email: email, profileImageLink: nil, documentId: userId)
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResults, error) in
+            
             if let error = error {
-              print(error.localizedDescription)
+                print(error)
+                self.userSessionAccountdelegate?.didReceiveError(self, error: error)
             }
-        })
-      }
-    
-  }
-  }
-  public func getCurrentUser() -> User?{
-    return Auth.auth().currentUser
-  }
-  public func signOut(){
-    do{
-      try Auth.auth().signOut()
-      userSessionSignOutDelegate?.didSignOutUser(_userSession: self)
-    }catch{
-      userSessionSignOutDelegate?.didReceiveSignOutError(self, error: error)
-    }
-  }
-  public func signInExistingUser(email:String,password:String){
-    Auth.auth().signIn(withEmail: email, password: password) { (results, error) in
-      if let error = error{
-        self.userSignInDelegate?.didReceiveSignInError(self, error: error)
-      }
-      else if let results = results{
-        self.userSignInDelegate?.didSignInUser(self, user: results.user)
-      }
-    }
-  }
-  public func updateExistingUser(imageURL:URL?,userName:String?,hairType:String?,bio:String?){
-    guard let user = getCurrentUser() else {
-      print("no user logged in")
-      return
-    }
-    let request = user.createProfileChangeRequest()
-    request.displayName = userName
-    request.photoURL = imageURL
-    request.commitChanges { (error) in
-      if let error = error{
-        print("\(error.localizedDescription)")
-      } else{
-      
-        DataBaseManager.firebaseDB.collection(FirebaseCollectionKeys.users)
-        .document(user.uid)
-          .updateData(["imageURL": imageURL?.absoluteString,
-                       "userName": userName ?? "no userName found",
-                       "hairType": hairType ?? "No hair type found",
-                       "bio":bio ?? "no bio found"], completion: { (error) in
-            guard let error = error else {
-              print("sucessful")
-              return
+            
+            if let authDataResults = authDataResults {
+                self.userSessionAccountdelegate?.didCreateAccount(self, user: authDataResults.user)
+                
+                do {
+                    try DataBaseManager.firebaseDB.collection(FirebaseCollectionKeys.users).document(userId).setData(from: user)
+                } catch {
+                    print(error)
+                }
             }
-             print("updating photo url error: \(error.localizedDescription)")
-          })
-      }
+        }
     }
-  }
-  public func deleteProduct(){
-    guard let user = getCurrentUser() else {return}
     
-  }
-  
+    func getCurrentUser() -> User? {
+        return Auth.auth().currentUser
+    }
+    
+    func signOut() {
+        do{
+            try Auth.auth().signOut()
+            userSessionSignOutDelegate?.didSignOutUser(_userSession: self)
+        }catch{
+            userSessionSignOutDelegate?.didReceiveSignOutError(self, error: error)
+        }
+    }
+    
+    func signInExistingUser(email:String,password:String){
+        Auth.auth().signIn(withEmail: email, password: password) { (results, error) in
+            if let error = error{
+                self.userSignInDelegate?.didReceiveSignInError(self, error: error)
+            }
+            else if let results = results{
+                self.userSignInDelegate?.didSignInUser(self, user: results.user)
+            }
+        }
+    }
 }

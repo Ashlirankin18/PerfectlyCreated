57d70491bbe257fd2c54bc9daa60ee42acf6733c
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-10-31 23:00:19 -0400

    Presented search controller

--- PerfectlyCreated/Product/ProductViewController.swift
+++ PerfectlyCreated/Product/ProductViewController.swift
@@ -14,6 +14,13 @@ final class ProductViewController: UIViewController {
 
     @IBOutlet private weak var addBarButtonItem: UIBarButtonItem!
     
+    private var searchController: SearchProductViewController = {
+        let controller = UIStoryboard(name: "SearchProductViewController", bundle: .main).instantiateViewController(identifier: "SearchProductViewController") { coder in
+            return SearchProductViewController(coder: coder)
+        }
+        return controller
+    }()
+    
     private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
@@ -39,8 +46,14 @@ final class ProductViewController: UIViewController {
             
         }
         
-       let searchProduct = UIAlertAction(title: "Search", style: .default) { _ in
-            
+       let searchProduct = UIAlertAction(title: "Search", style: .default) {[weak self] _ in
+        
+        guard let self = self else {
+            return
+        }
+        let controller  = UINavigationController(rootViewController: self.searchController)
+        controller.modalPresentationStyle = .fullScreen
+        self.present(controller, animated: true)
        }
         
         alertController.addAction(uploadProductAction)

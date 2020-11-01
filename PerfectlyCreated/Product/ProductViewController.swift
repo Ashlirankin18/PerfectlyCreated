19ae4c0cbed670e6f1cdd3a20a7fa873f370998d
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-11-01 13:37:03 -0500

    Uodated datasource

--- PerfectlyCreated/Product/ProductViewController.swift
+++ PerfectlyCreated/Product/ProductViewController.swift
@@ -11,8 +11,9 @@ import CombineCocoa
 import Combine
 
 final class ProductViewController: UIViewController {
-
+    
     @IBOutlet private weak var addBarButtonItem: UIBarButtonItem!
+    @IBOutlet private weak var productCollectionView: UICollectionView!
     
     private var searchController: SearchProductViewController = {
         let controller = UIStoryboard(name: "SearchProductViewController", bundle: .main).instantiateViewController(identifier: "SearchProductViewController") { coder in
@@ -23,9 +24,50 @@ final class ProductViewController: UIViewController {
     
     private var cancellables = Set<AnyCancellable>()
     
+    private var productsDictionary: [ProductModel] = [] {
+        didSet {
+            reloadDataSource(models: productsDictionary)
+        }
+    }
+    
+    private lazy var productManager = ProductManager()
+    
+    private lazy var dataSource: UICollectionViewDiffableDataSource<String, ProductModel> = UICollectionViewDiffableDataSource(collectionView: self.productCollectionView) { (_, indexPath, model) -> UICollectionViewCell? in
+        return self.configureCell(model: model, indexPath: indexPath)
+    }
+    
+    private let productCellCollectionLayoutSection: NSCollectionLayoutSection = {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.75), heightDimension: .fractionalWidth(0.6))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24)
+        return section
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       configureButtonTapHandler()
+        
+        productCollectionView.register(UINib(nibName: "ProductCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "ProductCollectionViewCell")
+        productCollectionView.collectionViewLayout = UICollectionViewCompositionalLayout(section: productCellCollectionLayoutSection)
+        productCollectionView.dataSource = dataSource
+        configureButtonTapHandler()
+        retrieveProducts()
+    }
+    
+    private func retrieveProducts() {
+        productManager.retrieveProducts { (result) in
+            switch result {
+            case let .failure(error):
+                print(error)
+            case let .success(models):
+                self.productsDictionary = models
+            }
+        }
     }
     
     private func configureButtonTapHandler() {
@@ -33,6 +75,16 @@ final class ProductViewController: UIViewController {
             self?.showAlertController()
         }
         .store(in: &cancellables)
+    }
+    
+    private func configureCell(model: ProductModel, indexPath: IndexPath) -> UICollectionViewCell {
+      
+        guard let cell = productCollectionView.dequeueReusableCell(withReuseIdentifier: "ProductCollectionViewCell", for: indexPath) as? ProductCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.productImageView.kf.setImage(with: URL(string: model.productImageURL))
+        cell.productNameLabel.text = model.productName
+        return cell
     }
     
     private func showAlertController() {
@@ -46,20 +98,34 @@ final class ProductViewController: UIViewController {
             
         }
         
-       let searchProduct = UIAlertAction(title: "Search", style: .default) {[weak self] _ in
-        
-        guard let self = self else {
-            return
+        let searchProduct = UIAlertAction(title: "Search", style: .default) {[weak self] _ in
+            
+            guard let self = self else {
+                return
+            }
+            let controller  = UINavigationController(rootViewController: self.searchController)
+            controller.modalPresentationStyle = .fullScreen
+            self.present(controller, animated: true)
         }
-        let controller  = UINavigationController(rootViewController: self.searchController)
-        controller.modalPresentationStyle = .fullScreen
-        self.present(controller, animated: true)
-       }
         
         alertController.addAction(uploadProductAction)
         alertController.addAction(scanBarCodeAction)
         alertController.addAction(searchProduct)
         
         present(alertController, animated: true)
+    }
+    
+    private func reloadDataSource(models: [ProductModel]) {
+        var snapshot = NSDiffableDataSourceSnapshot<String, ProductModel>()
+        
+        let sections = Set(models.map { $0.category })
+
+        snapshot.appendSections(Array(sections))
+        
+        sections.forEach { section in
+            let model = models.filter { $0.category == section }
+             snapshot.appendItems(model, toSection: section)
+        }
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }

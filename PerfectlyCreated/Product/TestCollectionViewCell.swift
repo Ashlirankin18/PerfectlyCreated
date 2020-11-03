2e5602d89da28ab0a672f97ffd82a010527e98a2
Ashli Rankin <ashlirankin@Ashlis-MacBook-Pro.local>
2020-11-02 21:36:10 -0500

    Test cell

---
+++ PerfectlyCreated/Product/TestCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TestCollectionViewCell.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 11/2/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

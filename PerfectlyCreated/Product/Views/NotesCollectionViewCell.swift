4039ad1a7e1f660015fa1fd3fa694beb5c0d59c9
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2020-11-25 18:03:43 -0500

    Created a cell to add notes

---
+++ PerfectlyCreated/Product/Views/NotesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NotesCollectionViewCell.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 11/8/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+final class NotesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var notesTextView: UITextView!
+    
+}

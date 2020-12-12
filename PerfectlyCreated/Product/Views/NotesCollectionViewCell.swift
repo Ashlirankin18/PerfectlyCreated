08fa1fbc5fd9fd01986413a34b1bbda58b154f1a
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2020-12-12 13:43:44 -0500

    Configured the notes collection view cell

--- PerfectlyCreated/Product/Views/NotesCollectionViewCell.swift
+++ PerfectlyCreated/Product/Views/NotesCollectionViewCell.swift
@@ -9,8 +9,25 @@
 import UIKit
 
 final class NotesCollectionViewCell: UICollectionViewCell {
-
+    
+    /// Contains the information needed to configure `NotesCollectionViewCell`.
+    struct ViewModel {
+        
+        /// The title of the cell.
+        let title: String
+        
+        /// The notes the user has added to the product.
+        let notes: String
+    }
+    
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var notesTextView: UITextView!
     
+    /// Single point of configuration of `NotesCollectionViewCell`.
+    var viewModel: ViewModel? {
+        didSet {
+            titleLabel.text = viewModel?.title
+            notesTextView.text = viewModel?.notes
+        }
+    }
 }

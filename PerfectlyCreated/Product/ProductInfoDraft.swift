0f491dfeb30a28d316443234de103b79177d5c1e
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-13 11:26:38 -0500

    Added product Info draft

---
+++ PerfectlyCreated/Product/ProductInfoDraft.swift
@@ -0,0 +1,19 @@
+//
+//  ProductInfoDraft.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 1/13/21.
+//  Copyright © 2021 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class ProductInfoDraft {
+    
+    var documentId: String = ""
+    
+    @Published var notes = ""
+    
+    @Published var isCompleted = false
+}

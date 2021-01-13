f1fde893e729d6709c94d22b3e38961c1c027e75
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-13 13:33:57 -0500

    subscribed to the publisher

--- PerfectlyCreated/BarcodeScanner/BarcodeScannerViewController.swift
+++ PerfectlyCreated/BarcodeScanner/BarcodeScannerViewController.swift
@@ -7,13 +7,72 @@
 //
 
 import UIKit
+import Combine
 
-class BarcodeScannerViewController: UIViewController {
+final class BarcodeScannerViewController: UIViewController {
     
-    private lazy var videoSession = VideoSessionController(backgroundView: view)
-   
+    @IBOutlet private weak var barcodeView: UIView!
+    
+    private lazy var videoSession = VideoSessionController(backgroundView: barcodeView)
+    
+    private var cancelButton = UIBarButtonItem(image: UIImage(systemName: "multiply"), style: .done, target: nil, action: nil)
+    
+    private var allHairProducts = [AllHairProducts]()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCancelButton()
+        allHairProducts = ProductDataManager.getProducts().sorted{$0.results.name < $1.results.name}
+        
+        videoSession.bacodeStringPublisher
+            .first()
+            .sink { [weak self] result in
+                switch result {
+                    case let .failure(error):
+                        self?.showAlert(title: "Error!", message: error.localizedDescription)
+                    case .finished: break
+                }
+            } receiveValue: { [weak self] barcodeString in
+                
+                guard let self = self else {
+                    return
+                }
+                let product = self.allHairProducts.first { $0.results.name == barcodeString }
+                
+                if let productUnwrapped = product {
+                    
+                    let productController =
+                        UIStoryboard(name: ProductDetailViewController.defaultNibName, bundle: .main).instantiateViewController(identifier: ProductDetailViewController.defaultNibName) { coder in
+                            return ProductDetailViewController(coder: coder, productType: .general(productUnwrapped))
+                        }
+                    self.present(productController, animated: true)
+                } else {
+                    self.showAlert(title: "Product Not Found", message: "Could not find product in our database.", style: .alert) {
+                        self.dismiss(animated: true)
+                    }
+                }
+            }.store(in: &cancellables)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         videoSession.configureCaptureDevice()
+    }
+    
+    // MARK: - BarcodeScannerViewController
+    
+    private func configureCancelButton() {
+        cancelButton.tintColor = .white
+        navigationItem.leftBarButtonItem = cancelButton
+        navigationController?.configuresShadowlessTransparentNavigationBar(backgroundColor: .black)
+        
+        cancelButton.tapPublisher.sink { _ in
+            self.dismiss(animated: true  )
+        }
+        .store(in: &cancellables)
     }
 }

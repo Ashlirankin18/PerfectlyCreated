03d939ff745359df62f4e903a8d14d61fba35cc4
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-15 12:15:54 -0500

    Updated docs

--- PerfectlyCreated/Model/ItemAttributes.swift
+++ PerfectlyCreated/Model/ItemAttributes.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
+/// Represents the product attributes.
 struct ItemAttributes: Codable, Equatable, Hashable {
-   
-    let title, upc, category: String
+    
+    /// The title of the product.
+    let title: String
+    
+    /// The barcode number.
+    let upc: String
+    
+    /// The category of the product.
+    let category: String
+    
+    /// The product image url
     let image: String
+    
+    /// the product description.
     let itemAttributesDescription: String
     
     enum CodingKeys: String, CodingKey {

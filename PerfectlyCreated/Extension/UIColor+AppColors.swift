e71a1b15e69f80162eb94bbbfaa379d0019cc509
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-15 15:17:05 -0500

    Updated app icon

---
+++ PerfectlyCreated/Extension/UIColor+AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+AppColors.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 1/15/21.
+//  Copyright © 2021 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var appPurple: UIColor = UIColor(named: "appPurple") ?? .black
+
+}

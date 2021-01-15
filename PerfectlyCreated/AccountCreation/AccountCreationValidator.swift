adad6c4760780da3e11f68d6770a53d5ee812035
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-15 09:33:49 -0500

    Updated the docs in AccountCreationValidator

--- PerfectlyCreated/AccountCreation/AccountCreationValidator.swift
+++ PerfectlyCreated/AccountCreation/AccountCreationValidator.swift
@@ -9,20 +9,26 @@
 import Foundation
 import Combine
 
+/// An object that stores intermediate state of the user account.
 final class AccountCreationValidator {
     
+    /// A published property for raw text of the email text field.
     @Published var emailText = ""
     
+    /// A published property for raw text of the password text field.
     @Published var passwordText = ""
     
+    /// A published property for raw text of the username text field.
     @Published var usernameText = ""
     
+    /// / A Publisher that represents if the data is ready to be submitted.
     var readyToSubmit: AnyPublisher<Bool, Never> {
         return $emailText.combineLatest($passwordText)
             .map { !$0.isEmpty && !$1.isEmpty }
             .eraseToAnyPublisher()
     }
     
+    /// Formats the user's email to create their username.
     func formatUserName() {
         guard !emailText.isEmpty, let index = emailText.firstIndex(of: "@") else {
             return

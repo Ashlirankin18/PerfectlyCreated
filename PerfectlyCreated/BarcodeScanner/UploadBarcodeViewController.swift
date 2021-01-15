cae0fca5f0e6798b32fe56bfdb288490c89474cd
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-15 13:01:45 -0500

    Updated methods

--- PerfectlyCreated/BarcodeScanner/UploadBarcodeViewController.swift
+++ PerfectlyCreated/BarcodeScanner/UploadBarcodeViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 import Combine
 
+/// `UIViewController` subclass which allows the user to upload a bar code.
 final class UploadBarcodeViewController: UIViewController {
     
     @IBOutlet private weak var chosenImageImageView: UIImageView!
-    
     @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var saveBarButtonItem: UIBarButtonItem!
     @IBOutlet private weak var backBarButtonItem: UIBarButtonItem!
@@ -21,6 +21,7 @@ final class UploadBarcodeViewController: UIViewController {
     
     private lazy var barcodeController = BarCodeScannerController()
     
+    /// Publisher which sends barcode changes.
     var bacodeStringPublisher: AnyPublisher<String, Never> {
         return bacodeStringSubject.eraseToAnyPublisher()
     }
@@ -31,6 +32,10 @@ final class UploadBarcodeViewController: UIViewController {
     
     private var cancellables = Set<AnyCancellable>()
     
+    /// Creates a new instance of `UploadBarcodeViewController`.
+    /// - Parameters:
+    ///   - coder: An abstract class that serves as the basis for objects that enable archiving and distribution of other objects.
+    ///   - chosenImage: The chosen image.
     init?(coder: NSCoder, chosenImage: UIImage) {
         self.chosenImage = chosenImage
         super.init(coder: coder)
@@ -40,34 +45,55 @@ final class UploadBarcodeViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureBarButtonItems()
+        configureScrollView()
+        navigationController?.configuresShadowlessTransparentNavigationBar(backgroundColor: .black)
+        configureSaveButton()
+        configureBackButton()
+    }
+    
+    // MARK: - UploadBarcodeViewController
+    
+    private func configureBarButtonItems() {
         backBarButtonItem.tintColor = .white
         saveBarButtonItem.tintColor = .white
+    }
+    
+    private func configureScrollView() {
         chosenImageImageView.image = chosenImage
         scrollView.minimumZoomScale = 0.5
         scrollView.maximumZoomScale = 2
         
         cropView.borderWidth = 3.0
         cropView.borderColor = .black
-        
-        navigationController?.configuresShadowlessTransparentNavigationBar(backgroundColor: .black)
+    }
+    
+    private func configureSaveButton() {
         saveBarButtonItem.tapPublisher.sink { [weak self] _ in
             guard let self = self else {
                 return
             }
-            self.barcodeController.captureOutout(with: self.chosenImage) { result in
-                switch result {
-                    case let .failure(error):
-                        self.showAlert(title: "Error", message: error.localizedDescription)
-                    case let .success(barcodeString):
-                        self.bacodeStringSubject.send(barcodeString)
-                        self.navigationController?.popViewController(animated: true)
+            self.barcodeController.captureOutput(with: .image(image: self.chosenImage))
+                .sink { [weak self] completion in
+                    switch completion {
+                        case let .failure(error):
+                            self?.showAlert(title: "Error", message: error.localizedDescription)
+                        case .finished: break
+                    }
+                } receiveValue: { [weak self] barcodeString in
+                    self?.bacodeStringSubject.send(barcodeString)
+                    self?.navigationController?.popViewController(animated: true)
                 }
-            }
+                .store(in: &self.cancellables)
         }
         .store(in: &cancellables)
-        
+    }
+    
+    private func configureBackButton() {
         backBarButtonItem.tapPublisher.sink { [weak self] _ in
             self?.navigationController?.popViewController(animated: true)
         }
@@ -76,6 +102,8 @@ final class UploadBarcodeViewController: UIViewController {
 }
 
 extension UploadBarcodeViewController: UIScrollViewDelegate {
+    
+    // MARK: - UIScrollViewDelegate
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return chosenImageImageView

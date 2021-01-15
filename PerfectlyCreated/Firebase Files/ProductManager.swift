6d330f62fe1a5d203c10fb274e33c9888ad3fe60
Ashli Rankin <ashlirankin18@gmail.com>
2021-01-15 09:43:57 -0500

    Updated documentation

--- PerfectlyCreated/Firebase Files/ProductManager.swift
+++ PerfectlyCreated/Firebase Files/ProductManager.swift
@@ -9,22 +9,27 @@
 import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+import FirebaseAuth
 
+/// Manages the networking logic related to the product.
 final class ProductManager {
     
-    let firebaseDB: Firestore = {
+    private let firebaseDB: Firestore = {
         let db = Firestore.firestore()
         let settings = db.settings
         db.settings = settings
         return db
     }()
     
-    private lazy var userManager = UserManager()
-    
+    /// The document id
     var documentId: String {
         return firebaseDB.collection(FirebaseCollectionKeys.users).document().documentID
     }
     
+    /// Adds a product to the database
+    /// - Parameters:
+    ///   - product: The product to be added.
+    ///   - completion: Called on completion of the network call.
     func addProduct(product: ProductModel, completion: @escaping (Result<Void, Error>) -> Void) {
         do {
             try firebaseDB.collection(FirebaseCollectionKeys.products).document(product.documentId).setData(from: product)
@@ -36,6 +41,11 @@ final class ProductManager {
         }
     }
     
+    /// Adds a hair product retrieve from the barcode lookup api to the database.
+    /// - Parameters:
+    ///   - documentId: The new document id.
+    ///   - product: The product to be added.
+    ///   - completion: Called on completion of the network logic.
     func addProduct(documentId: String?, product: HairProduct, completion: @escaping (Result<Void, Error>) -> Void) {
         guard let documentId = documentId else {
             return
@@ -51,9 +61,11 @@ final class ProductManager {
         }
     }
     
+    /// Retrieves all the products from the database.
+    /// - Parameter completion: Called on completion of the network call.
     func retrieveProducts(completion: @escaping (Result<[ProductModel], Error>) -> Void) {
         
-        guard let currentUser = userManager.currentUser else {
+        guard let currentUser =  Auth.auth().currentUser else {
             return
         }
         
@@ -74,6 +86,10 @@ final class ProductManager {
         }
     }
     
+    /// Retrieves a specific product from the database.
+    /// - Parameters:
+    ///   - documenId: The document id of the product.
+    ///   - completion: Called on completion of the newwork call.
     func retireveProduct(with documenId: String, completion: @escaping (Result<ProductModel, Error>) -> Void) {
         firebaseDB.collection(FirebaseCollectionKeys.products).document(documenId).addSnapshotListener({ (snapshot, error) in
            
@@ -96,6 +112,10 @@ final class ProductManager {
         })
     }
     
+    /// Deletes a product from the database.
+    /// - Parameters:
+    ///   - product: The product to be deleted.
+    ///   - completionHandler: Called on completion of the network call.
     func deleteProduct(_ product: ProductModel, completionHandler: @escaping (Result<Void, Error>) -> Void ) {
         firebaseDB.collection(FirebaseCollectionKeys.products).document(product.documentId).delete { error in
             if let error = error {
@@ -106,6 +126,11 @@ final class ProductManager {
         }
     }
     
+    /// Updates a product.
+    /// - Parameters:
+    ///   - documentId: The product document id.
+    ///   - productFields: The fields to be updated.
+    ///   - completion: Called on completion of the network call.
     func updateProduct(documentId: String, productFields: [String: Any], completion: @escaping ((Result<Void, Error>) -> Void)) {
         firebaseDB.collection(FirebaseCollectionKeys.products).document(documentId).updateData(productFields) { error in
             if let error = error {

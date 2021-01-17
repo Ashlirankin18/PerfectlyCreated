0e3bc0ea1610d5fdb1d4d4a96af5c77c70eb5312
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2021-01-16 20:51:14 -0500

    Added onboarding screen.

---
+++ PerfectlyCreated/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingViewController.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 1/16/21.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+    @IBAction private func createButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}

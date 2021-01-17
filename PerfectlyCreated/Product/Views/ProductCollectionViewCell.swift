6857ba35289948c596b67be8e114a6fb599a5c7b
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2021-01-16 20:24:15 -0500

    Added method for shadow

--- PerfectlyCreated/Product/Views/ProductCollectionViewCell.swift
+++ PerfectlyCreated/Product/Views/ProductCollectionViewCell.swift
@@ -13,5 +13,21 @@ final class ProductCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
+ 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureCell()
+    }
     
+    private func configureCell() {
+          layer.cornerRadius = frame.height / 2
+          layer.masksToBounds = false
+          contentView.layer.cornerRadius = 25
+          contentView.layer.masksToBounds = true
+          layer.shadowColor = UIColor.appPurple.withAlphaComponent(0.3).cgColor
+          layer.shadowOffset = CGSize(width: 0, height: 2)
+          layer.shadowOpacity = 1.0
+          
+          self.layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+      }
 }

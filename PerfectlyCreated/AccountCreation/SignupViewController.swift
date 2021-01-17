ec241ef5072047613adbe36217ec6f709abe8f89
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2021-01-16 21:20:21 -0500

    Updated docs

--- PerfectlyCreated/AccountCreation/SignupViewController.swift
+++ PerfectlyCreated/AccountCreation/SignupViewController.swift
@@ -30,7 +30,7 @@ final class SignupViewController: UIViewController {
     
     private var cancellables = Set<AnyCancellable>()
     
-    /// Creates a new instanc of `SignupViewController`.
+    /// Creates a new instance of `SignupViewController`.
     /// - Parameters:
     ///   - coder: An abstract class that serves as the basis for objects that enable archiving and distribution of other objects.
     ///   - accountFlow: The account flow.
@@ -60,10 +60,10 @@ final class SignupViewController: UIViewController {
     
     private func configureViews() {
         switch accountFlow {
-            case .signIn:
-                emailTextField.isHidden = true
-            case .signUp:
-                emailTextField.isHidden = false
+        case .signIn:
+            emailTextField.isHidden = true
+        case .signUp:
+            emailTextField.isHidden = false
         }
     }
     
@@ -102,9 +102,9 @@ final class SignupViewController: UIViewController {
             }
             self.userSession.signInExistingUser(email: self.accountCreationValidator.emailText, password: self.accountCreationValidator.passwordText).sink { error in
                 switch error {
-                    case let .failure(error):
-                        self.showAlert(title: "Error!", message: "There was an error logging in: \(error.localizedDescription)")
-                    case .finished: break
+                case let .failure(error):
+                    self.showAlert(title: "Error!", message: "There was an error logging in: \(error.localizedDescription)")
+                case .finished: break
                 }
             } receiveValue: { [weak self] _ in
                 guard let self = self else {
@@ -129,11 +129,13 @@ final class SignupViewController: UIViewController {
                 .sink(receiveCompletion: { error in
                     switch error {
                     case let .failure(error):
-                            self.showAlert(title: "Error!", message: "There was an error logging in: \(error.localizedDescription)")
+                        self.showAlert(title: "Error!", message: "There was an error logging in: \(error.localizedDescription)")
                     case .finished: break
                     }
                 }, receiveValue: { [weak self] _ in
-                    let controller = PerfectlyCraftedTabBarViewController()
+                    let controller = UIStoryboard(name: OnboardingViewController.defaultNibName, bundle: .main).instantiateViewController(identifier: OnboardingViewController.defaultNibName) {  coder in
+                        return OnboardingViewController(coder: coder)
+                    }
                     controller.modalPresentationStyle = .fullScreen
                     self?.show(controller, sender: self)
                 }).store(in: &self.cancellables)

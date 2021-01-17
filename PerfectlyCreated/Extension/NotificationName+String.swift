cbb601258efcda73801dbec2a0734dc7dbb1b3c1
Ashli Rankin <ashlirankin@Ashlis-MBP.fios-router.home>
2021-01-16 22:23:03 -0500

    Added notification name ext

---
+++ PerfectlyCreated/Extension/NotificationName+String.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName+String.swift
+//  PerfectlyCreated
+//
+//  Created by Ashli Rankin on 1/16/21.
+//  Copyright © 2021 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let signupSuccessfullyCompleted: Notification.Name = .init("signupSuccessfullyCompleted")
+}

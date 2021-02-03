6fa2f0fe7fb45bd26acbbde04f2bb8cf7c35940f
Ashli Rankin <ashlirankin18@gmail.com>
2021-02-03 16:22:40 -0500

    Removed unused method

--- PerfectlyCreated/SearchController/SearchProductViewController.swift
+++ PerfectlyCreated/SearchController/SearchProductViewController.swift
@@ -78,10 +78,6 @@ final class SearchProductViewController: UIViewController {
         }
         .store(in: &cancellables)
     }
-    
-    @objc private func backButtonPressed() {
-        self.dismiss(animated: true)
-    }
 }
 
 extension SearchProductViewController: UITableViewDataSource {

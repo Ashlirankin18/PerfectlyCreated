09f49ff3f9f5a5ab8bb9681a2928cf677e6b8926
Ashli Rankin <ashlirankin18@gmail.com>
2021-03-29 19:29:19 -0400

    Initialized controller with view

--- PerfectlyCreated/BarcodeScanner/VideoSessionController.swift
+++ PerfectlyCreated/BarcodeScanner/VideoSessionController.swift
@@ -27,6 +27,13 @@ final class VideoSessionController: NSObject {
         return barcodeStringSubject.eraseToAnyPublisher()
     }
     
+    private let view: UIView
+    
+    init(view: UIView) {
+        self.view = view
+        super.init()
+    }
+    
     private var barcodeStringSubject = PassthroughSubject<String, Error>()
     
     private var cancellables = Set<AnyCancellable>()
@@ -40,7 +47,7 @@ final class VideoSessionController: NSObject {
     }
     
     /// Configures the AvCaptureDevice.
-    func configureCaptureDevice(with view: UIView) {
+    func configureCaptureDevice() {
         startRunningSession()
     
         let metadataOutput = AVCaptureMetadataOutput()
@@ -54,26 +61,26 @@ final class VideoSessionController: NSObject {
         session.addInput(deviceInput)
         session.addOutput(metadataOutput)
         
-        configureOutput(view: view)
+        configureOutput()
         
         metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
-        metadataOutput.rectOfInterest = aVCaptureVideoPreviewLayer?.metadataOutputRectConverted(fromLayerRect: CGRect(x: view.frame.width * 0.068, y: view.center.y, width: view.frame.width * 0.85, height: view.frame.width * 0.5)) ?? .zero
+        metadataOutput.rectOfInterest = aVCaptureVideoPreviewLayer?.metadataOutputRectConverted(fromLayerRect: CGRect(x: view.frame.width * 0.065, y: view.center.y - 48, width: view.frame.width * 0.85, height: view.frame.width * 0.5)) ?? .zero
     }
     
-    private func configureOutput(view: UIView) {
+    private func configureOutput() {
         aVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
         aVCaptureVideoPreviewLayer?.frame = view.frame
         aVCaptureVideoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         
         if let previewLayerUnwrap = aVCaptureVideoPreviewLayer {
             view.layer.addSublayer(previewLayerUnwrap)
-            configureRoundRectPath(view: view)
+            configureRoundRectPath()
         }
     }
     
-    private func configureRoundRectPath(view: UIView) {
-        let path = UIBezierPath(roundedRect: CGRect(x: view.frame.width * 0.068, y: view.center.y, width: view.frame.width * 0.85, height: view.frame.width * 0.5), cornerRadius: 20.0)
+    private func configureRoundRectPath() {
+        let path = UIBezierPath(roundedRect: CGRect(x: view.frame.width * 0.068, y: view.center.y - 48, width: view.frame.width * 0.85, height: view.frame.width * 0.5), cornerRadius: 20.0)
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineWidth = 5.0
         shapeLayer.strokeColor = UIColor.white.cgColor
